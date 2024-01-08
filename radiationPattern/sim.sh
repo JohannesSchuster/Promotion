2199446a37185081b78659f1dee703d65f15cc40
Johannes Schuster <johannes.schuster@physik.uni-regensburg.de>
2024-01-08 21:00:48 +0100

    moved results into results folder

--- radiationPattern/sim.sh
+++ radiationPattern/sim.sh
@@ -2,7 +2,7 @@
 
 for h in 2 4 6 8 10 12 14; do
     for r in 0.25 0.5 0.75 1 2 3; do
-        ./a.out $h 1e-6 0.15 c 0.15 $r >> "log_circle_h${h}cm_r${r}cm.txt" &
-        ./a.out $h 1e-6 0.15 g 0.15 $r >> "log_gauss_h${h}cm_r${r}cm.txt" &
+        ./a.out $h 1e-6 0.15 c 0.15 $r >> "results/log_circle_h${h}cm_r${r}cm.txt" &
+        ./a.out $h 1e-6 0.15 g 0.15 $r >> "results/log_gauss_h${h}cm_r${r}cm.txt" &
     done
 done
